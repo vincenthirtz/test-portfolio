145f0a7e878abb7d4bce84774556a02dac0fd3e4
Vincent Hirtz <vincent.hirtz@sapiendo.fr>
2023-01-27 18:49:48 +0100

    change home visual too

--- src/components/ProjectHomePage/index.tsx
+++ src/components/ProjectHomePage/index.tsx
@@ -96,62 +96,65 @@ const ProjectHomePage = (props: PropsType): JSX.Element => {
       {projects.map((project, index) => {
         return (
           // On met la classe active au projet qui est en train d'être affiché et on l'enlève sinon
+
           <div
             key={index}
             className={`${styles.container__project__item} ${
               actualProject == index ? styles.active : ''
             }`}
           >
-            <div className={styles.project__item}>
-              <div className={styles.project__title}>
-                <NavLink to={project.link} className={styles.project__link}>
-                  <h2 className={styles.animation__background}>{project.name}</h2>
-                </NavLink>
-                <div className={styles.index__project}>
-                  <span>
-                    <div className={styles.animation__width}></div>
-                  </span>
-                  <p className={styles.animation__transform_top_small}>{'0' + (index + 1)}</p>
-                </div>
-              </div>
-              <div className={styles.item__container}>
-                <div className={styles.left__container}>
-                  <div className={styles.services__container}>
-                    <p
-                      className={`${styles.project__services} ${styles.animation__transform_top_small}`}
-                    >
-                      {project.featuring}
-                    </p>
-                    <p
-                      className={`${styles.project__services} ${styles.animation__transform_top_small}`}
-                    >
-                      {project.services}
-                    </p>
-                    <p
-                      className={`${styles.project__year} ${styles.animation__transform_top_small}`}
-                    >
-                      {project.year}
-                    </p>
-                  </div>
-                  <span className={styles.container__image}>
-                    <img
-                      className={styles.animation__width}
-                      src={project.image}
-                      alt={project.alt}
-                    />
-                  </span>
-                </div>
-                <div className={styles.right__container}>
-                  <div
-                    className={`${styles.container__number_of_projects} ${styles.animation__transform_left_rotate}`}
-                  >
-                    <p className={styles.animation__transform_left}>{index + 1}</p>
-                    <span></span>
-                    <p className={styles.animation__transform_right}>{projects.length}</p>
+            <NavLink to={project.link}>
+              <div className={styles.project__item}>
+                <div className={styles.project__title}>
+                  <NavLink to={project.link} className={styles.project__link}>
+                    <h2 className={styles.animation__background}>{project.name}</h2>
+                  </NavLink>
+                  <div className={styles.index__project}>
+                    <span>
+                      <div className={styles.animation__width}></div>
+                    </span>
+                    <p className={styles.animation__transform_top_small}>{'0' + (index + 1)}</p>
                   </div>
                 </div>
+                <div className={styles.item__container}>
+                  <div className={styles.left__container}>
+                    <div className={styles.services__container}>
+                      <p
+                        className={`${styles.project__services} ${styles.animation__transform_top_small}`}
+                      >
+                        {project.featuring}
+                      </p>
+                      <p
+                        className={`${styles.project__services} ${styles.animation__transform_top_small}`}
+                      >
+                        {project.services}
+                      </p>
+                      <p
+                        className={`${styles.project__year} ${styles.animation__transform_top_small}`}
+                      >
+                        {project.year}
+                      </p>
+                    </div>
+                    <span className={styles.container__image}>
+                      <img
+                        className={styles.animation__width}
+                        src={project.image}
+                        alt={project.alt}
+                      />
+                    </span>
+                  </div>
+                  <div className={styles.right__container}>
+                    <div
+                      className={`${styles.container__number_of_projects} ${styles.animation__transform_left_rotate}`}
+                    >
+                      <p className={styles.animation__transform_left}>{index + 1}</p>
+                      <span></span>
+                      <p className={styles.animation__transform_right}>{projects.length}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
+            </NavLink>
           </div>
         );
       })}
