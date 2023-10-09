f6681ab32779176f46a2df1fba781ebe4574cd09
vincenthirtz <31092476+vincenthirtz@users.noreply.github.com>
2023-10-09 20:59:58 +0200

    resolve images

--- src/components/HeaderProject/index.tsx
+++ src/components/HeaderProject/index.tsx
@@ -13,16 +13,11 @@ type PropsType = {
 
 const HeaderProject = (props: PropsType): JSX.Element => {
   // Props pour changer le header des projets en fonction de la page
-  const { title, index, titleLink, link, year, services, elements } = props;
+  const { title, titleLink, link, year, services, elements } = props;
 
   return (
     <div className={styles.container}>
       <div className={styles.title}>
-        <div className={`${styles.number_of_projects} ${styles.animation__transform_left_rotate}`}>
-          <p className={styles.animation__transform_left}>{index}</p>
-          <span></span>
-          <p className={styles.animation__transform_right}>5</p>
-        </div>
         <h1 className={styles.animation__background}>{title}</h1>
       </div>
       <div className={styles.about}>
